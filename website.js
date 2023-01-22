accbbea4c97644ec45fe4ede1f6c1e3670138edc
Hasan <waulathan@gmail.com>
2023-01-22 20:20:32 +0700

    project

---
+++ website.js
@@ -0,0 +1,7 @@
+function redirInstagram() {
+    window.location.href = "https://instagram.com/hanwaulat"
+}
+
+function redirWhatsapp() {
+    window.location.href = "https://wa.me/+6285254308418"
+}
